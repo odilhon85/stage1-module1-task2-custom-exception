536251aeda25d2fae2bd9113a9ef37e1fa817f1f
odilhon85 <i.o.r@inbox.ru>
2022-09-01 10:37:06 +0500

    1-attempt

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,17 +5,30 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID) throws StudentNotFoundException{
+      return Student.getValueOf(studentID);
   }
 
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
 
     for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+      try{
+        Student student = manager.find(IDs[i]);
+        System.out.println("Student name " + student.getName());
+      }catch (Exception e){
+        System.out.println("Could not find student with ID "+ IDs[i]);
+      }
     }
 
   }
+}
+
+class StudentNotFoundException extends IllegalArgumentException{
+
+  StudentNotFoundException (String message){
+    super(message);
+  }
+
+
 }
